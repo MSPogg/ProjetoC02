7061839a7f9204211137522270479dabde63cf69
MSPogg <murilopoggettosilva@gmail.com>
2025-11-05 22:55:11 -0300

    Update projeto_c02.cpp
    05/11

--- projeto_c02.cpp
+++ projeto_c02.cpp
@@ -5,7 +5,19 @@
 #include <string>
 #include <windows.h>
 using namespace std;
-
+ 
+void exibirMenu()
+{
+	cout << "              LOCACAO DE CARROS              " << endl;
+	cout << "1 | Alugar carro" << endl;
+	cout << "2 | Carros alugados" << endl;
+	cout << "3 | Devolver carro" << endl;
+	cout << "4 | Cadastrar carros" << endl;
+	cout << "5 | Sair" << endl;
+	cout << endl;
+	cout << "Escolha a opcao desejada" << endl;
+}
+ 
 // Struct (Marcelo)
 struct Carros
 {
@@ -14,9 +26,9 @@ struct Carros
 	float precoDia;
 	bool disponivel;
 };
-
+ 
 // Função Cadastrar Carro (Marcelo)
-Carros CadastrarCarro()
+/*Carros CadastrarCarro()
 {
 	Carros carro;
 	cout << "Digite o modelo do carro:" << endl;
@@ -24,64 +36,96 @@ Carros CadastrarCarro()
 	cout << "Digite a placa do carro:" << endl;
 	getline(cin >> ws, carro.placa);
 	cout << "Digite o valor do aluguel:" << endl
-		 << "R$: ";
+<< "R$: ";
 	cin >> carro.precoDia;
 	return carro;
 }
-
+ 
 // Funções Menu (Wendel)
-void menuA()
+/* void menuA()
 {
-	string y;
-	int cod;
-
+	string CarrosDisponiveisParaLocacao;
+ 
 	ifstream arq;
 	arq.open("carrosDisponiveis.txt", ifstream::in);
-
+ 
 	while (!arq.eof())
 	{
-		getline(arq, y);
-		cout << y << endl;
+		getline(arq, CarrosDisponiveisParaLocacao);
+		cout << CarrosDisponiveisParaLocacao << endl;
 	}
-
-	cout << "digite o codigo do carro desejado" << endl;
-	cin >> cod;
-
+ 
+	//funçao do Murilo "entra" aqui, o "cout" de status e "cin" do modelo que o cliente que escolher vai estar na função dele //
+ 
 	arq.close();
+} */
+ 
+void alugarCarro(Carros carro[])
+{
+	string escolha;
+	bool encontrado = false;
+ 
+	cout << "Modelo que gostaria de alugar: ";
+	getline(cin >> ws, escolha);
+ 
+	for (int i = 0; i < 50; i++)
+	{
+		if (carro[i].modelo == escolha)
+		{
+			encontrado = true;
+ 
+			if (carro[i].disponivel)
+			{
+				carro[i].disponivel = false;
+				cout << "Carro alugado com sucesso!" << endl;
+			}
+			else
+			{
+				cout << "Esse carro já está alugado." << endl;
+			}
+ 
+			break;
+		}
+	}
+ 
+	if(!encontrado)
+	{
+		cout << "Nenhum carro encontrado";
+	}
 }
+ 
 void menuB()
 {
-	string w;
-	string t;
-
+	string CarrosAlugados;
+ 
 	ifstream arq;
 	arq.open("carrosAlugados.txt", ifstream::in);
-
+ 
 	while (!arq.eof())
 	{
-		getline(arq, w);
-		cout << w << endl;
+		getline(arq, CarrosAlugados);
+		cout << CarrosAlugados << endl;
 	}
-
+ 
 	arq.close();
 }
 void menuC()
 {
-	int d;
-	cout << "Digite o codigo do carro para realizar a devolucao" << endl;
+	string DevolverCarro;
+	cout << "Digite o modelo do veículo para realizar a devolucao" << endl;
 	cout << endl;
-	cin >> d;
+	cin >> DevolverCarro;
 	cout << endl;
-	cout << "Devolucao cadastrada com sucesso - Agrademos a preferencia" << endl;
+	cout << "Devolucao cadastrada com sucesso - Agradecemos a preferencia" << endl;
 }
 void menuD()
 {
-	string e, f, p;
+	string Email, Senha, nQuantidadesDeCarros;
 	cout << "=Login=" << endl;
-	while (e != "wendel.gabriel@inatel.br")
+	while (Email != "wendel.gabriel@inatel.br")
 	{
-		cin >> e;
-		if (e == "wendel.gabriel@inatel.br")
+		cin >> Email;
+		if (Email == "wendel.gabriel@inatel.br")
 		{
 			cout << "Digite sua senha" << endl;
 		}
@@ -90,11 +134,11 @@ void menuD()
 			cout << "Login incorreto, tente novamente" << endl;
 		}
 	}
-
-	while (f != "123nnn")
+ 
+	while (Senha != "123nnn")
 	{
-		cin >> f;
-		if (f != "123nnn")
+		cin >> Senha;
+		if (Senha != "123nnn")
 		{
 			cout << "Senha incorreta, tente novamente" << endl;
 		}
@@ -102,68 +146,45 @@ void menuD()
 	cout << "             CENTRAL DE CADASTRO             " << endl;
 	cout << endl;
 	cout << "Quantos carros deseja cadastrar?" << endl;
-
-	cin >> p;
+ 
+	cin >> nQuantidadesDeCarros;
 }
-void exibirMenu()
-{
-	cout << "              LOCACAO DE CARROS              " << endl;
-	cout << "1 | Carros disponiveis para locacao" << endl;
-	cout << "2 | Carros alugados" << endl;
-	cout << "3 | Devolver carro" << endl;
-	cout << "4 | Cadastrar carros" << endl;
-	cout << "5 | Sair" << endl;
-	cout << endl;
-	cout << "Escolha a opcao desejada" << endl;
-}
-
+ 
 // Função Listar Carro (Wendel)
-//...
-
+ 
 // Funções Alugar e Devolver Carro (Murilo)
-void alugarCarro(Carros carro[])
-{
-	string escolha;
-	/*
-	primeiramente   necessario a funcao de listarCarros() rodar para o usuario
-	verificar qual modelo gostaria de alugar
-	*/
-
-	cout << "Modelo que gostaria de alugar: ";
-	getline(cin >> ws, escolha);
-
-	for (int i = 0; i < 50; i++)
-	{
-		if (escolha == carro[i].modelo)
-		{
-			// Coloca a bool disponivel como falso
-			carro[i].disponivel = 0;
-			break;
-		}
-	}
-}
 void devolverCarro()
 {
 }
-
+ 
 // Main
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8); // Para saídas com acento
-
-	int x = 0;
+ 
+	int OpcaoDeEscolhaNoMenu = 0;
 	Carros carro[50];
-
+	ifstream arqLeitura;
+	ofstream arqEscrita;
+	string carrosDisponiveis;
+ 
 	exibirMenu();
-
-	while (x != 5)
+ 
+	while (OpcaoDeEscolhaNoMenu != 5)
 	{
-		cin >> x;
-
-		switch (x)
+		cin >> OpcaoDeEscolhaNoMenu;
+ 
+		switch (OpcaoDeEscolhaNoMenu)
 		{
 		case 1:
-			menuA();
+			arqLeitura.open ("carrosDisponiveis.txt");
+			while (!arqLeitura.eof())
+			{
+				getline(arqLeitura, carrosDisponiveis);
+				cout << carrosDisponiveis << endl;
+			}
+			alugarCarro(carro);
+			arqLeitura.close();
 			break;
 		case 2:
 			menuB();
